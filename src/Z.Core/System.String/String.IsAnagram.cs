638de84fd1272164bd098940638b7849b4fdf509
Sayyid Ayoub Kokabi <auob.kokabi@gmail.com>
2015-06-21 18:05:04 +0430

    Revert "Revert "Add extension method.""
    This reverts commit 21488b3349e5097178f1ec1a5818ffe95de65336.

---
+++ src/Z.Core/System.String/String.IsAnagram.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2015 ZZZ Projects. All rights reserved
+// Licensed under MIT License (MIT) (https://github.com/zzzprojects/Z.ExtensionMethods)
+// Website: http://www.zzzprojects.com/
+// Feedback / Feature Requests / Issues : http://zzzprojects.uservoice.com/forums/283927
+// All ZZZ Projects products: Entity Framework Extensions / Bulk Operations / Extension Methods /Icon Library
+
+using System;
+using System.Linq;
+
+public static partial class Extensions
+{
+    /// <summary>
+    ///     A string extension method that query if '@this' is anagram of other String.
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <param name="otherString">The other string</param>
+    /// <returns>true if the @this is anagram of the otherString, false if not.</returns>
+    public static bool IsAnagram(this string @this, string otherString)
+    {
+        return @this
+            .OrderBy(c => c)
+            .SequenceEqual(otherString.OrderBy(c => c));
+    }
+}
