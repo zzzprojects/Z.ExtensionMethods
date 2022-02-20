cf74244d1214c0a0c5cc488c93953526298f106c
Rui Lima <3x0dv5@users.noreply.github.com>
2022-02-20 14:11:26 +0000

    Add overload to allow spaces as alphanumeric
    add extension method to allow spaces to be considered alphanumeric

--- src/Z.Core/System.String/String.IsAlphaNumeric.cs
+++ src/Z.Core/System.String/String.IsAlphaNumeric.cs
@@ -3,7 +3,7 @@
 // Issues: https://github.com/zzzprojects/Z.ExtensionMethods/issues
 // License (MIT): https://github.com/zzzprojects/Z.ExtensionMethods/blob/master/LICENSE
 // More projects: https://zzzprojects.com/
-// Copyright © ZZZ Projects Inc. All rights reserved.
+// Copyright Â© ZZZ Projects Inc. All rights reserved.
 using System.Text.RegularExpressions;
 
 public static partial class Extensions
@@ -16,5 +16,18 @@ public static partial class Extensions
     public static bool IsAlphaNumeric(this string @this)
     {
         return !Regex.IsMatch(@this, "[^a-zA-Z0-9]");
+    }
+    
+    /// <summary>
+    ///     A string extension method that query if '@this' is Alphanumeric.
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <param name="spacesAllowed">alphanumeric characters or spaces.</param>
+    /// <returns>true if Alphanumeric, false if not.</returns>
+    public static bool IsAlphaNumeric(this string @this, bool spacesAllowed)
+    {       
+        if (spacesAllowed)
+            return !Regex.IsMatch(@this, @"[^a-zA-Z0-9\s]");
+        return IsAlphaNumeric(@this);
     }
 }
